c2e10d955ab9fadbc2397c45643422d09f742961
Matej Kormuth <dobrakmato@gmail.com>
2015-04-29 20:06:42 +0200

    updated license to bsd-2 clause; fixes #1

--- client/src/main/scripts/run.bat
+++ client/src/main/scripts/run.bat
@@ -1,19 +1,28 @@
 @REM
 @REM client - Multiplayer Java game engine.
-@REM Copyright (C) 2015 Matej Kormuth <http://www.github.com/dobrakmato>
+@REM Copyright (c) 2015, Matej Kormuth <http://www.github.com/dobrakmato>
+@REM All rights reserved.
 @REM
-@REM This program is free software: you can redistribute it and/or modify
-@REM it under the terms of the GNU General Public License as published by
-@REM the Free Software Foundation, either version 3 of the License, or
-@REM (at your option) any later version.
+@REM Redistribution and use in source and binary forms, with or without modification,
+@REM are permitted provided that the following conditions are met:
 @REM
-@REM This program is distributed in the hope that it will be useful,
-@REM but WITHOUT ANY WARRANTY; without even the implied warranty of
-@REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-@REM GNU General Public License for more details.
+@REM 1. Redistributions of source code must retain the above copyright notice, this
+@REM list of conditions and the following disclaimer.
 @REM
-@REM You should have received a copy of the GNU General Public License
-@REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@REM 2. Redistributions in binary form must reproduce the above copyright notice,
+@REM this list of conditions and the following disclaimer in the documentation and/or
+@REM other materials provided with the distribution.
+@REM
+@REM THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+@REM ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+@REM WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+@REM DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+@REM ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+@REM (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+@REM LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+@REM ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+@REM (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+@REM SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 @REM
 
 java -jar -Djava.library.path="${release.natives}" "${finalName}"
